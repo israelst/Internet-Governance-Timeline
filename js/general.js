ba6b6231f1570ceb4a78da59bb919caf07b5bb4c
Israel Teixeira <israelst@gmail.com>
2014-07-07 23:59:26 -0300

    Init calendar view

--- js/general.js
+++ js/general.js
@@ -7,6 +7,60 @@ window.addEventListener('load', function(){
     };
 
     slide.onchange();
+
+    var width = 960,
+        height = 136,
+        cellSize = 17; // cell size
+
+    var day = d3.time.format("%w"),
+        week = d3.time.format("%U"),
+        format = d3.time.format("%Y-%m-%d");
+
+    var svg = d3.select("body").selectAll("svg")
+        .data(d3.range(2012, 2015))
+      .enter().append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("class", "RdYlGn")
+      .append("g")
+        .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
+
+    svg.append("text")
+        .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
+        .style("text-anchor", "middle")
+        .text(function(d) { return d; });
+
+    var rect = svg.selectAll(".day")
+        .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+      .enter().append("rect")
+        .attr("class", "day")
+        .attr("width", cellSize)
+        .attr("height", cellSize)
+        .attr("x", function(d) { return week(d) * cellSize; })
+        .attr("y", function(d) { return day(d) * cellSize; })
+        .datum(format);
+
+    rect.append("title")
+        .text(function(d) { return d; });
+
+    svg.selectAll(".month")
+        .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+      .enter().append("path")
+        .attr("class", "month")
+        .attr("d", monthPath);
+
+    function monthPath(t0) {
+      var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+          d0 = +day(t0), w0 = +week(t0),
+          d1 = +day(t1), w1 = +week(t1);
+      return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
+          + "H" + w0 * cellSize + "V" + 7 * cellSize
+          + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
+          + "H" + (w1 + 1) * cellSize + "V" + 0
+          + "H" + (w0 + 1) * cellSize + "Z";
+    }
+
+
     d3.json("data/data.json", function(data){
         data = preprocessing(data);
 
@@ -67,6 +121,19 @@ window.addEventListener('load', function(){
             event_class = event_class || 'other';
             return event_class;
         }
+
+    var startDates = data.map(function(d){return format(d.date[0]);}),
+        endDates = data.map(function(d){return format(d.date[1]);}),
+        count = {};
+    startDates.concat(endDates).forEach(function(d){
+        count[d] |= 0;
+        count[d]++;
     });
+    rect.filter(function(d) { return d in count; })
+        .style("fill", function(d) { return count[d]; })
+        .select("title");
+
+    });
+
 }, false);
 
