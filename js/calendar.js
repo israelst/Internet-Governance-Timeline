eb2dde5b277c274589b840044406e3f2855525e1
Israel Teixeira <israelst@gmail.com>
2014-07-16 22:54:21 -0300

    Change saturation instead of "redness"

--- js/calendar.js
+++ js/calendar.js
@@ -74,8 +74,8 @@ function calendarChart(){
         var count = eventsByDay(data);
         daysRects.filter(function(d) { return d in count; })
             .style("fill", function(d) {
-                var r = 255 - count[d] * 100,
-                    color = d3.rgb(r, 20, 20);
+                var saturation  = 255 - count[d] * 100,
+                    color = d3.rgb(saturation, saturation, saturation);
                 return color.toString();
             });
     };
