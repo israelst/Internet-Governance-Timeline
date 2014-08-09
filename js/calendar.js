4448e9d7e031a6ab941b5014eae07e900b319f7d
Israel Teixeira <israelst@gmail.com>
2014-08-08 23:12:49 -0300

    Fix indentation

--- js/calendar.js
+++ js/calendar.js
@@ -6,9 +6,9 @@ function calendarChart(){
         dayRects;
 
     function monthPath(t0) {
-      var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
-          d0 = +day(t0), w0 = +week(t0),
-          d1 = +day(t1), w1 = +week(t1);
+        var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+            d0 = +day(t0), w0 = +week(t0),
+            d1 = +day(t1), w1 = +week(t1);
       return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
           + "H" + w0 * cellSize + "V" + 7 * cellSize
           + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
