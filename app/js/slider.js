124155b8a35c92958765f1b3dbf183dbdb4cb262
Israel Teixeira <israelst@gmail.com>
2014-09-08 11:30:05 -0300

    Remove exceeding space

--- app/js/slider.js
+++ app/js/slider.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function(){
     var slide = document.getElementById('slide'),
         chosen = document.getElementById('chosen');
 
-    slide.onchange = function (){
+    slide.onchange = function(){
         chosen.textContent = this.value;
     };
     slide.onchange();
