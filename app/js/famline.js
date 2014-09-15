a63c43f39a34c20f3631e639623f6d51d3730cad
Israel Teixeira <israelst@gmail.com>
2014-09-15 20:09:03 -0300

    Add a default min width to scale

--- app/js/famline.js
+++ app/js/famline.js
@@ -2,14 +2,20 @@ var d3 = require('d3'),
     domainOfDates = require('./date.js').domainOfDates;
 
 exports.FamlineChart = function(){
+    var minWidth = 500;
+
     function chart(selection){
         var extent = domainOfDates(selection.data()),
-            width = selection.node().ownerSVGElement.width.baseVal.value,
-            x = d3.time.scale().domain(extent).range([0, width]),
+            svg = selection.node().ownerSVGElement,
+            width = svg.width.baseVal.value,
+            x = d3.time.scale().domain(extent),
             maxDuration = d3.max(
                     selection.data().map(function(d){
                         return d.duration;
                     }));
+
+            width = Math.max(width, minWidth);
+            x.range([0, width]);
 
         function cx(d){
             return (x(d.date[0]) + x(d.date[1]))/2;
