6e884501675bd64a38b4e1c650dbf4d30c6de30b
Israel Teixeira <israelst@gmail.com>
2014-09-22 20:07:21 -0300

    Refactoring to prepare split chart in focus + context

--- app/js/famline.js
+++ app/js/famline.js
@@ -1,21 +1,14 @@
 var d3 = require('d3'),
     dates = require('./date.js').domainOfDates;
 
-exports.FamlineChart = function(){
-    var minWidth = 800;
+function Circles(){
+    var biggerRadius;
+    function circles(selection){
+        var data = selection.datum(),
+            width = selection.node().parentNode.width.baseVal.value,
+            x = d3.time.scale().range([0, width]).domain(dates(data));
 
-    function chart(svg){
-        var data = svg.datum(),
-            width = Math.max(svg.attr('width'), minWidth),
-            x = d3.time.scale().range([0, width]).domain(dates(data)),
-            biggerRadius = d3.max(data.map(radius)),
-            kinds = d3.set(data.map(kind)).values(),
-            height = (biggerRadius * 3) * kinds.length,
-            y = d3.scale.ordinal().rangePoints([0, height]).domain(kinds);
-
-        function kind(d){
-            return d.institutions;
-        }
+        biggerRadius = d3.max(data.map(radius));
 
         function cx(d){
             return (x(d.date[0]) + x(d.date[1]))/2;
@@ -25,11 +18,7 @@ exports.FamlineChart = function(){
             return cx(d) - x(d.date[0]);
         }
 
-        svg.attr('height', height)
-            .attr('width', width);
-
-        svg.append('g')
-            .attr('class', 'all-events')
+        return selection
             .selectAll('circle')
             .data(data)
             .enter()
@@ -39,18 +28,46 @@ exports.FamlineChart = function(){
             .attr('cy', biggerRadius)
             .attr('r', radius);
 
-        var groupedByKind = svg.append('g')
-            .attr('class', 'events-by-kind')
-            .attr('transform', 'translate(0,' + biggerRadius * 4 + ')');
+    }
 
-        groupedByKind.selectAll('circle')
-            .data(data)
-            .enter()
-            .append('circle')
-            .style('fill-opacity', 0.3)
-            .attr('cx', cx)
-            .attr('cy', function(d){ return y(d.institutions);})
-            .attr('r', radius);
+    circles.height = function(){
+        return biggerRadius * 2;
+    };
+
+    return circles;
+}
+
+exports.FamlineChart = function(){
+    var minWidth = 800;
+
+    function chart(svg){
+        var width = Math.max(svg.attr('width'), minWidth),
+            data = svg.datum(),
+            kinds = d3.set(data.map(kind)).values(),
+            groupedByKind = svg.append('g').attr('class', 'events-by-kind'),
+            circlesChart = Circles(),
+            circles = circlesChart(groupedByKind),
+            height = (circlesChart.height() * 1.5) * kinds.length,
+            y = d3.scale.ordinal().rangePoints([0, height]).domain(kinds);
+
+        function kind(d){
+            return d.institutions;
+        }
+
+        function cy(d){
+            return y(d.institutions);
+        }
+
+        circles.attr('cy', cy);
+        groupedByKind.attr('transform', 'translate(0,' + circlesChart.height() * 2 + ')');
+
+        svg.append('g')
+            .attr('class', 'all-events')
+            .call(Circles());
+
+        svg.attr('height', height * 2)
+            .attr('width', width);
+
 
         var yAxis = d3.svg.axis().scale(y).orient('right')
              .tickValues(y.domain())
