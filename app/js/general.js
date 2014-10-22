fe46317ff624fe1afc68dfbe3ccd9c76010c6c98
Israel Teixeira <israelst@gmail.com>
2014-10-22 19:37:52 -0200

    Remove doubled code

--- app/js/general.js
+++ app/js/general.js
@@ -57,13 +57,7 @@ function eventsByDay(data){
 
 window.addEventListener('load', function(){
     var slide = document.getElementById('slide'),
-        chosen = document.getElementById('chosen'),
         calendar = calendarChart(d3.range(2013, 2015));
-
-    slide.onchange = function (){
-        chosen.textContent = this.value;
-    };
-    slide.onchange();
 
     d3.select('#calendar-view').call(calendar);
 
